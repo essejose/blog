ddec880f34ba089f849e680038db75c57a750238
jose <ze_pk2@hotmail.com>
2019-09-19 20:10:34 -0300

    struture styled

---
+++ src/components/Layout/styled.js
@@ -0,0 +1,11 @@
+import style from "styled-components"
+
+export const LayoutWrapper = style.section`
+  display: flex,
+`
+export const LayoutMain = style.main`
+  background: #16202c;
+  min-height:100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`
